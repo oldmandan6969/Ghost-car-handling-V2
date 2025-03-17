283b8c513d2e1e89a5ab8829c44545cf18836056
DEVGHOSTIE <74836846+oldmandan6969@users.noreply.github.com>
2025-03-17 15:26:16 +0000

    done

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'AshiroDev'
+version "1.0"
+
+files {
+    'carcols.meta',
+    'carvariations.meta',
+	'data/**/vehicles.meta',
+    'data/**/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE'			'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'carvariations.meta'
